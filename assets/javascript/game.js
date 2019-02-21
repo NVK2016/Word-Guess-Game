65283743ebbb671c172fc71b68ab1000d07c8740
Namita Shenai <namita.shenai@gmail.com>
2019-02-21 10:03:46 -0800

    Blank html & js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,8 @@
 // Game logic 
+
+//Create a list of words "
+var hangmanList = ["apple", "peach"]; 
+
+// Randomly chooses a choice from the options array. This is the Computer's guess.
+var currentWord = hangmanList[Math.floor(Math.random() * hangmanList.length)];
+console.log(currentWord); 
